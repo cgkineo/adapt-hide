6ff936f2914e7f633b99447df58f6e19ff5b1740
Matt Leathes <matt.leathes@kineo.com>
2020-05-26 12:26:38 +0100

    Update for FW v5 compatibility (#4)
    * switch to v5 css class name
    
    * add missing semi-colon; double quotes to single
    
    * indentation: 4 spaces to 2
    
    * use of _.bindAll is slight overkill for just one function
    
    * bump version, fw dependency
    
    * Fix typo, spacing amends
    
    * Added entry for _isDynamic
    
    * _trickle > _hide
    
    * various corrections to README: 
    `display-none` to `u-display-none`
    remove double-backticks
    delete know issues section as all entries referenced stuff no longer supported (IE8/9) or used (force load)
    
    Co-authored-by: Guy Willis <guy.willis@kineo.com>

--- js/adapt-hide.js
+++ js/adapt-hide.js
@@ -1,108 +1,107 @@
 define([
-    "core/js/adapt"
+  'core/js/adapt'
 ], function(Adapt) {
 
-    var Hide = Backbone.Controller.extend({
+  var Hide = Backbone.Controller.extend({
 
-        postRenderEvent: ["article", "block", "component", ""].join("View:postRender "),
-        managedViews: [],
-        $html: null,
+    postRenderEvent: ['article', 'block', 'component', ''].join('View:postRender '),
+    managedViews: [],
+    $html: null,
 
-        initialize: function() {
-            $(function() {
-                this.$html = $("html");
-            }.bind(this))
+    initialize: function() {
+      $(function() {
+        this.$html = $('html');
+      }.bind(this));
 
-            this.listenTo(Adapt, this.postRenderEvent, this.onPostRender);
-            this.listenTo(Adapt, {
-                "pageView:ready": this.onPageReady,
-                "device:changed": this.checkStates
-            });
+      this.listenTo(Adapt, this.postRenderEvent, this.onPostRender);
+      this.listenTo(Adapt, {
+        'pageView:ready': this.onPageReady,
+        'device:changed': this.checkStates
+      });
 
-            _.bindAll(this, "triggerResize");
-            this.triggerResize = _.debounce(this.triggerResize, 250);
-        },
+      this.triggerResize = _.debounce(this.triggerResize.bind(this), 250);
+    },
 
-        onPostRender: function(view) {
-            var model = view.model;
-            var config = model.get("_hide");
+    onPostRender: function(view) {
+      var model = view.model;
+      var config = model.get('_hide');
 
-            if (!config || !config._isEnabled) return;
-            
-            this.listenTo(model, "change:_isInteractionComplete", this.onComplete);
-            this.managedViews.push(view);
-            this.checkState(view);
-        },
-        
-        onComplete: function(model, val) {
-            var config = model.get("_hide");
+      if (!config || !config._isEnabled) return;
 
-            if (!config || !config._isEnabled) return;
-            if (!config._proxyId) return;
-            
-            var proxyModel = Adapt.findById(config._proxyId);
-            if (!proxyModel) return;
-            proxyModel.setOnChildren("_isComplete", val);
-        },
+      this.listenTo(model, 'change:_isInteractionComplete', this.onComplete);
+      this.managedViews.push(view);
+      this.checkState(view);
+    },
 
-        checkState: function(view) {
-            var model = view.model;
-            var config = model.get("_hide");
+    onComplete: function(model, val) {
+      var config = model.get('_hide');
 
-            if (!config._isEnabled) return;
-            if (config._isDynamic === false && config._isEvaluated) return;
+      if (!config || !config._isEnabled) return;
+      if (!config._proxyId) return;
 
-            config._isEvaluated = true;
+      var proxyModel = Adapt.findById(config._proxyId);
+      if (!proxyModel) return;
+      proxyModel.setOnChildren('_isComplete', val);
+    },
 
-            var hideOnClasses = config._onClasses;
-            var hideOnMediaQuery = config._onMediaQuery;
+    checkState: function(view) {
+      var model = view.model;
+      var config = model.get('_hide');
 
-            var isHiddenOnClasses = !hideOnClasses || (hideOnClasses && this.$html.is(hideOnClasses));
-            var isHiddenOnMediaQuery = !hideOnMediaQuery || (hideOnMediaQuery && window.matchMedia && window.matchMedia(hideOnMediaQuery).matches);
+      if (!config._isEnabled) return;
+      if (config._isDynamic === false && config._isEvaluated) return;
 
-            var isHidden = (isHiddenOnClasses && isHiddenOnMediaQuery);
+      config._isEvaluated = true;
 
-            if (isHidden) {
-                model.setOnChildren("_isAvailable", false);
-                view.$el.addClass("display-none");
-                return;
-            }
+      var hideOnClasses = config._onClasses;
+      var hideOnMediaQuery = config._onMediaQuery;
 
-            model.setOnChildren("_isAvailable", true);
-            view.$el.removeClass("display-none");
+      var isHiddenOnClasses = !hideOnClasses || (hideOnClasses && this.$html.is(hideOnClasses));
+      var isHiddenOnMediaQuery = !hideOnMediaQuery || (hideOnMediaQuery && window.matchMedia && window.matchMedia(hideOnMediaQuery).matches);
 
-            this.triggerResize();
+      var isHidden = (isHiddenOnClasses && isHiddenOnMediaQuery);
 
-        },
+      if (isHidden) {
+        model.setOnChildren('_isAvailable', false);
+        view.$el.addClass('u-display-none');
+        return;
+      }
 
-        onPageReady: function() {
-            this.checkStates();
-        },
+      model.setOnChildren('_isAvailable', true);
+      view.$el.removeClass('u-display-none');
 
-        checkStates: function() {
-            if (!this.managedViews.length) return;
+      this.triggerResize();
 
-            this.managedViews.forEach(function(view) {
-                this.checkState(view);
-            }.bind(this));
+    },
 
-            Adapt.trigger("pageLevelProgress:update");
-        },
+    onPageReady: function() {
+      this.checkStates();
+    },
 
-        triggerResize: function() {
-            $(window).resize();
-        },
+    checkStates: function() {
+      if (!this.managedViews.length) return;
 
-        remove: function() {
-            this.managedViews.forEach(function(view) {
-                view.model.setOnChildren("_isAvailable", true);
-                this.stopListening(view.model);
-            }.bind(this));
-            this.managedViews.length = 0;
-        }
+      this.managedViews.forEach(function(view) {
+        this.checkState(view);
+      }.bind(this));
 
-    });
+      Adapt.trigger('pageLevelProgress:update');
+    },
 
-    return new Hide();
+    triggerResize: function() {
+      $(window).resize();
+    },
+
+    remove: function() {
+      this.managedViews.forEach(function(view) {
+        view.model.setOnChildren('_isAvailable', true);
+        this.stopListening(view.model);
+      }.bind(this));
+      this.managedViews.length = 0;
+    }
+
+  });
+
+  return new Hide();
 
 });
